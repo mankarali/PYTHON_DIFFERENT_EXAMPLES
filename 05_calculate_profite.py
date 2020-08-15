99bde68a307c0479c14c2ec1a0aa125c78a50d4d
mustafa <mustafaankarali35@gmail.com>
2020-08-15 16:57:22 -0500

    05_calculate_profite.py added.

---
+++ 05_calculate_profite.py
@@ -0,0 +1,42 @@
+"""
+Calculate the Profit
+You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. 
+You are given a dictionary containing the cost price per unit (in dollars), sell price per unit (in dollars), 
+and the starting inventory. Return the total profit made, rounded to the nearest dollar.
+
+Examples
+profit({
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+}) ➞ 14796
+
+profit({
+  "cost_price": 225.89,
+  "sell_price": 550.00,
+  "inventory": 100
+}) ➞ 32411
+
+profit({
+  "cost_price": 2.77,
+  "sell_price": 7.95,
+  "inventory": 8500
+}) ➞ 44030
+Notes
+Assume all inventory has been sold.
+Profit = Total Sales - Total Cost
+"""
+
+def profit(info):
+    
+    c = list(info.values())[0]
+    s = list(info.values())[1]
+    i = list(info.values())[2]
+    p = (s-c)*i
+    return (round(p))
+    
+profit({
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+})    
